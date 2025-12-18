765495b95621cf19915913947a0a494d94b9b12f
andan <andan@KLORAN>
2025-12-18 15:11:16 +0100

    Completed JWT authentication flow with UserDetailsService and security filter

--- spring-backend/src/main/java/be/vives/taskmanager/config/SecurityConfig.java
+++ spring-backend/src/main/java/be/vives/taskmanager/config/SecurityConfig.java
@@ -1,20 +1,21 @@
 package be.vives.taskmanager.config;
 
+import be.vives.taskmanager.infrastructure.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http, JwtAuthenticationFilter jwtAuthenticationFilter) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sm ->
@@ -22,9 +23,11 @@ public class SecurityConfig {
                 )
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/projects/**").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers("/projects/**").hasRole("USER")
                         .anyRequest().authenticated()
+                )
+                .addFilterBefore(
+                        jwtAuthenticationFilter,
+                        UsernamePasswordAuthenticationFilter.class
                 );
 
         return http.build();
