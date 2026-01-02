cddf9c1f6aecf97504e2ce70ee59361d3dd20190
andan <andan@KLORAN>
2026-01-03 00:07:11 +0100

    Deployed frontend(Angular), backend(java spring boot) and database (MySQL) on Kubernetes with seeded data

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
     production: true,
     //apiUrl: 'http://backend:8080/api'
-    apiUrl: 'http://localhost:8080/api'
+    apiUrl: 'http://localhost:8080/api'   //for port-forwading(using clusterIP in backend service.yaml) testing
+    //apiUrl: 'http://127.0.0.1:6881/api'
+    //apiUrl: 'http://192.168.49.2:30080/api'  //for testing using nodeport in backend service.yaml
 };
