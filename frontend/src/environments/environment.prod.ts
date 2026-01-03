9da4a8945a96ffc28092020ae9876d4f0e22c915
andan <andan@KLORAN>
2026-01-03 04:37:59 +0100

    Connected Cloudflare domain; app + API accessible over HTTPS

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
     production: true,
     //apiUrl: 'http://backend:8080/api'
-    apiUrl: 'http://localhost:8080/api'   //for port-forwading(using clusterIP in backend service.yaml) testing
+    //apiUrl: 'http://localhost:8080/api'   //for port-forwading(using clusterIP in backend service.yaml) testing
     //apiUrl: 'http://127.0.0.1:6881/api'
     //apiUrl: 'http://192.168.49.2:30080/api'  //for testing using nodeport in backend service.yaml
+    apiUrl: 'https://api.kloran-taskmanager.org/api'
 };
